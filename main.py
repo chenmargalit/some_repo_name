1f64a589fdc4118148808045f7950b0b27f6bfda
chenmargalit <bakugan@gmail.com>
2024-02-13 04:35:00 +0200

    a

---
+++ main.py
@@ -0,0 +1,10 @@
+some_string = 'chair'
+
+string_length = len(some_string)
+
+if string_length > 10:
+    print('bigger')
+elif string_length < 10 and string_length > 5:
+    print('medium')
+else:
+    print('small')
